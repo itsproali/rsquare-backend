a7dcc4ab0ae88893ea980bcb9e19b5f10ce53a0c
Mohammad Ali <itsproali@gmail.com>
2022-11-09 01:57:41 +0600

    💥 Image Delete Added

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const path = require('path');
+const path = require("path");
 
 exports.uploadImage = async (req, res, next) => {
   try {
@@ -8,8 +8,7 @@ exports.uploadImage = async (req, res, next) => {
     const user = await User.findById(userId);
     const images = user.images;
     for (const img of inputted_image) {
-      console.log(img);
-      const temp_name = path.parse(img.originalname).name; 
+      const temp_name = path.parse(img.originalname).name;
       images.push({
         name: temp_name,
         path: img.filename,
@@ -36,6 +35,29 @@ exports.uploadImage = async (req, res, next) => {
       message: "Image Uploaded successfully",
       data: images,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteImage = async (req, res, next) => {
+  try {
+    const checked_image = req.body;
+    const userId = req.user.id;
+    const result = await User.updateOne(
+      { _id: userId },
+      { $pull: { images: { _id: { $in: checked_image } } } }
+    );
+
+    if (!result) {
+      return res
+        .status(400)
+        .send({ success: false, message: "Image couldn't deleted..!" });
+    }
+
+    res
+      .status(200)
+      .send({ success: true, message: "Images deleted successfully" });
   } catch (error) {
     next(error);
   }
--- routes/imageRoute.js
+++ routes/imageRoute.js
@@ -1,8 +1,9 @@
-const { uploadImage } = require("../controllers/imageController");
+const { uploadImage, deleteImage } = require("../controllers/imageController");
 const { verifyToken } = require("../middleware/token");
 const router = require("express").Router();
 const uploader = require("../middleware/uploader");
 
 router.patch("/upload", verifyToken, uploader.array("images"), uploadImage);
+router.patch("/delete", verifyToken, deleteImage);
 
 module.exports = router;
