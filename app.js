520a687a28adc88002a410b990eaa8f68c4a4ff2
Mohammad Ali <itsproali@gmail.com>
2022-12-19 21:35:22 +0600

    ⚡️ Back to the Previous Commit

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const userRoute = require("./routes/userRoute");
-// const imageRoute = require("./routes/imageRoute");
+const userRoute = require("./routes/userRoute");
+const imageRoute = require("./routes/imageRoute");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// // Default Route
-// app.get("/", (req, res) => {
-//   res.send("Welcome to Rsquare Server..!");
-// });
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Welcome to Rsquare Server..!");
+});
 
-// // Routes
-// app.use("/user", userRoute);
-// app.use("/image", imageRoute);
-// app.use("/images", express.static(__dirname + "/images/"));
+// Routes
+app.use("/user", userRoute);
+app.use("/image", imageRoute);
+app.use("/images", express.static(__dirname + "/images/"));
 
-// // Not Found Route
-// app.get("*", (req, res) => {
-//   res.sendStatus(404).send({ message: "Route Not Found" });
-// });
+// Not Found Route
+app.get("*", (req, res) => {
+  res.sendStatus(404).send({ message: "Route Not Found" });
+});
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+const express = require("express");
 require("colors");
 require("dotenv").config();
 const dbConnect = require("./utilities/dbConnect");
 const errorHandler = require("./middleware/errorHandler");
 const port = process.env.PORT || 5000;
 const app = require("./app");
-const router = require("./routes/router");
 
 // Connect to Database
 dbConnect();
-
-app.use("/", router);
 
 // Server
 app.listen(port, () => {
